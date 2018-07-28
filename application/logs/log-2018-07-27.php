1793f1874a30b4ffe813de02da4a8c65b79e953c
lnavarro <luis.navarro.arenas@gmail.com>
2018-07-28 00:46:47 -0400

    Instalación CI_HMVC, Crear blog 1ra parte

---
+++ application/logs/log-2018-07-27.php
@@ -0,0 +1,204 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2018-07-27 04:33:25 --> Config Class Initialized
+INFO - 2018-07-27 04:33:25 --> Hooks Class Initialized
+DEBUG - 2018-07-27 04:33:25 --> UTF-8 Support Enabled
+INFO - 2018-07-27 04:33:25 --> Utf8 Class Initialized
+INFO - 2018-07-27 04:33:25 --> URI Class Initialized
+DEBUG - 2018-07-27 04:33:25 --> No URI present. Default controller set.
+INFO - 2018-07-27 04:33:25 --> Router Class Initialized
+INFO - 2018-07-27 04:33:25 --> Output Class Initialized
+INFO - 2018-07-27 04:33:26 --> Security Class Initialized
+DEBUG - 2018-07-27 04:33:26 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 04:33:26 --> Input Class Initialized
+INFO - 2018-07-27 04:33:26 --> Language Class Initialized
+INFO - 2018-07-27 04:33:26 --> Language Class Initialized
+INFO - 2018-07-27 04:33:26 --> Config Class Initialized
+INFO - 2018-07-27 04:33:26 --> Loader Class Initialized
+INFO - 2018-07-27 04:33:26 --> Controller Class Initialized
+DEBUG - 2018-07-27 04:33:26 --> Welcome MX_Controller Initialized
+DEBUG - 2018-07-27 04:33:26 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/Welcome/views/welcome_message.php
+INFO - 2018-07-27 04:33:26 --> Final output sent to browser
+DEBUG - 2018-07-27 04:33:26 --> Total execution time: 0.4700
+INFO - 2018-07-27 04:37:12 --> Config Class Initialized
+INFO - 2018-07-27 04:37:12 --> Hooks Class Initialized
+DEBUG - 2018-07-27 04:37:12 --> UTF-8 Support Enabled
+INFO - 2018-07-27 04:37:12 --> Utf8 Class Initialized
+INFO - 2018-07-27 04:37:12 --> URI Class Initialized
+DEBUG - 2018-07-27 04:37:12 --> No URI present. Default controller set.
+INFO - 2018-07-27 04:37:12 --> Router Class Initialized
+INFO - 2018-07-27 04:37:12 --> Output Class Initialized
+INFO - 2018-07-27 04:37:12 --> Security Class Initialized
+DEBUG - 2018-07-27 04:37:12 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 04:37:12 --> Input Class Initialized
+INFO - 2018-07-27 04:37:12 --> Language Class Initialized
+INFO - 2018-07-27 04:37:12 --> Language Class Initialized
+INFO - 2018-07-27 04:37:12 --> Config Class Initialized
+INFO - 2018-07-27 04:37:12 --> Loader Class Initialized
+INFO - 2018-07-27 04:37:12 --> Controller Class Initialized
+DEBUG - 2018-07-27 04:37:12 --> Welcome MX_Controller Initialized
+DEBUG - 2018-07-27 04:37:12 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/Welcome/views/welcome_message.php
+INFO - 2018-07-27 04:37:12 --> Final output sent to browser
+DEBUG - 2018-07-27 04:37:12 --> Total execution time: 0.0400
+INFO - 2018-07-27 05:09:32 --> Config Class Initialized
+INFO - 2018-07-27 05:09:32 --> Hooks Class Initialized
+DEBUG - 2018-07-27 05:09:32 --> UTF-8 Support Enabled
+INFO - 2018-07-27 05:09:32 --> Utf8 Class Initialized
+INFO - 2018-07-27 05:09:32 --> URI Class Initialized
+DEBUG - 2018-07-27 05:09:33 --> No URI present. Default controller set.
+INFO - 2018-07-27 05:09:33 --> Router Class Initialized
+INFO - 2018-07-27 05:09:33 --> Output Class Initialized
+INFO - 2018-07-27 05:09:33 --> Security Class Initialized
+DEBUG - 2018-07-27 05:09:33 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 05:09:33 --> Input Class Initialized
+INFO - 2018-07-27 05:09:33 --> Language Class Initialized
+INFO - 2018-07-27 05:09:33 --> Language Class Initialized
+INFO - 2018-07-27 05:09:33 --> Config Class Initialized
+INFO - 2018-07-27 05:09:33 --> Loader Class Initialized
+INFO - 2018-07-27 05:09:33 --> Helper loaded: form_helper
+INFO - 2018-07-27 05:09:33 --> Helper loaded: url_helper
+INFO - 2018-07-27 05:13:39 --> Config Class Initialized
+INFO - 2018-07-27 05:13:39 --> Hooks Class Initialized
+DEBUG - 2018-07-27 05:13:39 --> UTF-8 Support Enabled
+INFO - 2018-07-27 05:13:39 --> Utf8 Class Initialized
+INFO - 2018-07-27 05:13:39 --> URI Class Initialized
+DEBUG - 2018-07-27 05:13:40 --> No URI present. Default controller set.
+INFO - 2018-07-27 05:13:40 --> Router Class Initialized
+INFO - 2018-07-27 05:13:40 --> Output Class Initialized
+INFO - 2018-07-27 05:13:40 --> Security Class Initialized
+DEBUG - 2018-07-27 05:13:40 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 05:13:40 --> Input Class Initialized
+INFO - 2018-07-27 05:13:40 --> Language Class Initialized
+INFO - 2018-07-27 05:13:40 --> Language Class Initialized
+INFO - 2018-07-27 05:13:40 --> Config Class Initialized
+INFO - 2018-07-27 05:13:40 --> Loader Class Initialized
+INFO - 2018-07-27 05:13:40 --> Helper loaded: form_helper
+INFO - 2018-07-27 05:13:40 --> Helper loaded: url_helper
+INFO - 2018-07-27 05:14:05 --> Config Class Initialized
+INFO - 2018-07-27 05:14:05 --> Hooks Class Initialized
+DEBUG - 2018-07-27 05:14:05 --> UTF-8 Support Enabled
+INFO - 2018-07-27 05:14:05 --> Utf8 Class Initialized
+INFO - 2018-07-27 05:14:05 --> URI Class Initialized
+DEBUG - 2018-07-27 05:14:05 --> No URI present. Default controller set.
+INFO - 2018-07-27 05:14:05 --> Router Class Initialized
+INFO - 2018-07-27 05:14:05 --> Output Class Initialized
+INFO - 2018-07-27 05:14:05 --> Security Class Initialized
+DEBUG - 2018-07-27 05:14:05 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 05:14:05 --> Input Class Initialized
+INFO - 2018-07-27 05:14:05 --> Language Class Initialized
+INFO - 2018-07-27 05:14:05 --> Language Class Initialized
+INFO - 2018-07-27 05:14:05 --> Config Class Initialized
+INFO - 2018-07-27 05:14:05 --> Loader Class Initialized
+INFO - 2018-07-27 05:14:05 --> Helper loaded: form_helper
+INFO - 2018-07-27 05:14:05 --> Helper loaded: url_helper
+INFO - 2018-07-27 05:14:08 --> Config Class Initialized
+INFO - 2018-07-27 05:14:08 --> Hooks Class Initialized
+DEBUG - 2018-07-27 05:14:08 --> UTF-8 Support Enabled
+INFO - 2018-07-27 05:14:08 --> Utf8 Class Initialized
+INFO - 2018-07-27 05:14:08 --> URI Class Initialized
+DEBUG - 2018-07-27 05:14:08 --> No URI present. Default controller set.
+INFO - 2018-07-27 05:14:08 --> Router Class Initialized
+INFO - 2018-07-27 05:14:08 --> Output Class Initialized
+INFO - 2018-07-27 05:14:08 --> Security Class Initialized
+DEBUG - 2018-07-27 05:14:08 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 05:14:08 --> Input Class Initialized
+INFO - 2018-07-27 05:14:08 --> Language Class Initialized
+INFO - 2018-07-27 05:14:08 --> Language Class Initialized
+INFO - 2018-07-27 05:14:08 --> Config Class Initialized
+INFO - 2018-07-27 05:14:08 --> Loader Class Initialized
+INFO - 2018-07-27 05:14:08 --> Helper loaded: form_helper
+INFO - 2018-07-27 05:14:08 --> Helper loaded: url_helper
+INFO - 2018-07-27 05:15:21 --> Config Class Initialized
+INFO - 2018-07-27 05:15:21 --> Hooks Class Initialized
+DEBUG - 2018-07-27 05:15:21 --> UTF-8 Support Enabled
+INFO - 2018-07-27 05:15:21 --> Utf8 Class Initialized
+INFO - 2018-07-27 05:15:21 --> URI Class Initialized
+DEBUG - 2018-07-27 05:15:21 --> No URI present. Default controller set.
+INFO - 2018-07-27 05:15:21 --> Router Class Initialized
+INFO - 2018-07-27 05:15:21 --> Output Class Initialized
+INFO - 2018-07-27 05:15:21 --> Security Class Initialized
+DEBUG - 2018-07-27 05:15:21 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 05:15:21 --> Input Class Initialized
+INFO - 2018-07-27 05:15:21 --> Language Class Initialized
+INFO - 2018-07-27 05:15:21 --> Language Class Initialized
+INFO - 2018-07-27 05:15:21 --> Config Class Initialized
+INFO - 2018-07-27 05:15:21 --> Loader Class Initialized
+INFO - 2018-07-27 05:15:21 --> Helper loaded: form_helper
+INFO - 2018-07-27 05:15:21 --> Helper loaded: url_helper
+INFO - 2018-07-27 05:15:21 --> Database Driver Class Initialized
+INFO - 2018-07-27 05:15:21 --> Controller Class Initialized
+DEBUG - 2018-07-27 05:15:21 --> Welcome MX_Controller Initialized
+DEBUG - 2018-07-27 05:15:21 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/Welcome/views/welcome_message.php
+INFO - 2018-07-27 05:15:21 --> Final output sent to browser
+DEBUG - 2018-07-27 05:15:21 --> Total execution time: 0.2500
+INFO - 2018-07-27 17:18:22 --> Config Class Initialized
+INFO - 2018-07-27 17:18:22 --> Hooks Class Initialized
+DEBUG - 2018-07-27 17:18:22 --> UTF-8 Support Enabled
+INFO - 2018-07-27 17:18:22 --> Utf8 Class Initialized
+INFO - 2018-07-27 17:18:22 --> URI Class Initialized
+DEBUG - 2018-07-27 17:18:22 --> No URI present. Default controller set.
+INFO - 2018-07-27 17:18:22 --> Router Class Initialized
+INFO - 2018-07-27 17:18:22 --> Output Class Initialized
+INFO - 2018-07-27 17:18:23 --> Security Class Initialized
+DEBUG - 2018-07-27 17:18:23 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 17:18:23 --> Input Class Initialized
+INFO - 2018-07-27 17:18:23 --> Language Class Initialized
+INFO - 2018-07-27 17:18:23 --> Language Class Initialized
+INFO - 2018-07-27 17:18:23 --> Config Class Initialized
+INFO - 2018-07-27 17:18:23 --> Loader Class Initialized
+INFO - 2018-07-27 17:18:23 --> Helper loaded: form_helper
+INFO - 2018-07-27 17:18:23 --> Helper loaded: url_helper
+INFO - 2018-07-27 17:18:23 --> Database Driver Class Initialized
+INFO - 2018-07-27 17:18:23 --> Controller Class Initialized
+DEBUG - 2018-07-27 17:18:23 --> Welcome MX_Controller Initialized
+DEBUG - 2018-07-27 17:18:23 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/Welcome/views/welcome_message.php
+INFO - 2018-07-27 17:18:23 --> Final output sent to browser
+DEBUG - 2018-07-27 17:18:23 --> Total execution time: 1.1300
+INFO - 2018-07-27 17:21:14 --> Config Class Initialized
+INFO - 2018-07-27 17:21:14 --> Hooks Class Initialized
+DEBUG - 2018-07-27 17:21:14 --> UTF-8 Support Enabled
+INFO - 2018-07-27 17:21:14 --> Utf8 Class Initialized
+INFO - 2018-07-27 17:21:14 --> URI Class Initialized
+DEBUG - 2018-07-27 17:21:14 --> No URI present. Default controller set.
+INFO - 2018-07-27 17:21:14 --> Router Class Initialized
+INFO - 2018-07-27 17:21:14 --> Output Class Initialized
+INFO - 2018-07-27 17:21:14 --> Security Class Initialized
+DEBUG - 2018-07-27 17:21:14 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 17:21:14 --> Input Class Initialized
+INFO - 2018-07-27 17:21:14 --> Language Class Initialized
+INFO - 2018-07-27 17:21:14 --> Language Class Initialized
+INFO - 2018-07-27 17:21:14 --> Config Class Initialized
+INFO - 2018-07-27 17:21:14 --> Loader Class Initialized
+INFO - 2018-07-27 17:21:14 --> Helper loaded: form_helper
+INFO - 2018-07-27 17:21:14 --> Helper loaded: url_helper
+INFO - 2018-07-27 17:21:14 --> Helper loaded: blog_helper
+INFO - 2018-07-27 17:21:14 --> Database Driver Class Initialized
+INFO - 2018-07-27 17:21:14 --> Controller Class Initialized
+DEBUG - 2018-07-27 17:21:14 --> Welcome MX_Controller Initialized
+DEBUG - 2018-07-27 17:21:14 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/Welcome/views/welcome_message.php
+INFO - 2018-07-27 17:21:14 --> Final output sent to browser
+DEBUG - 2018-07-27 17:21:14 --> Total execution time: 0.0600
+INFO - 2018-07-27 17:28:10 --> Config Class Initialized
+INFO - 2018-07-27 17:28:10 --> Hooks Class Initialized
+DEBUG - 2018-07-27 17:28:10 --> UTF-8 Support Enabled
+INFO - 2018-07-27 17:28:10 --> Utf8 Class Initialized
+INFO - 2018-07-27 17:28:10 --> URI Class Initialized
+DEBUG - 2018-07-27 17:28:10 --> No URI present. Default controller set.
+INFO - 2018-07-27 17:28:10 --> Router Class Initialized
+INFO - 2018-07-27 17:28:10 --> Output Class Initialized
+INFO - 2018-07-27 17:28:10 --> Security Class Initialized
+DEBUG - 2018-07-27 17:28:10 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-27 17:28:10 --> Input Class Initialized
+INFO - 2018-07-27 17:28:10 --> Language Class Initialized
+INFO - 2018-07-27 17:28:10 --> Language Class Initialized
+INFO - 2018-07-27 17:28:10 --> Config Class Initialized
+INFO - 2018-07-27 17:28:10 --> Loader Class Initialized
+INFO - 2018-07-27 17:28:10 --> Helper loaded: form_helper
+INFO - 2018-07-27 17:28:10 --> Helper loaded: url_helper
+INFO - 2018-07-27 17:28:10 --> Helper loaded: blog_helper
+INFO - 2018-07-27 17:28:10 --> Database Driver Class Initialized
+INFO - 2018-07-27 17:28:10 --> Controller Class Initialized
+DEBUG - 2018-07-27 17:28:10 --> Welcome MX_Controller Initialized
+DEBUG - 2018-07-27 17:28:10 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/Welcome/views/welcome_message.php
+INFO - 2018-07-27 17:28:10 --> Final output sent to browser
+DEBUG - 2018-07-27 17:28:10 --> Total execution time: 0.0820
---
+++ application/logs/log-2018-07-28.php
@@ -0,0 +1,80 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+INFO - 2018-07-28 05:24:46 --> Config Class Initialized
+INFO - 2018-07-28 05:24:46 --> Hooks Class Initialized
+DEBUG - 2018-07-28 05:24:46 --> UTF-8 Support Enabled
+INFO - 2018-07-28 05:24:46 --> Utf8 Class Initialized
+INFO - 2018-07-28 05:24:46 --> URI Class Initialized
+DEBUG - 2018-07-28 05:24:46 --> No URI present. Default controller set.
+INFO - 2018-07-28 05:24:46 --> Router Class Initialized
+INFO - 2018-07-28 05:24:47 --> Output Class Initialized
+INFO - 2018-07-28 05:24:47 --> Security Class Initialized
+DEBUG - 2018-07-28 05:24:47 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-28 05:24:47 --> Input Class Initialized
+INFO - 2018-07-28 05:24:47 --> Language Class Initialized
+INFO - 2018-07-28 05:24:47 --> Language Class Initialized
+INFO - 2018-07-28 05:24:47 --> Config Class Initialized
+INFO - 2018-07-28 05:24:47 --> Loader Class Initialized
+INFO - 2018-07-28 05:24:47 --> Helper loaded: form_helper
+INFO - 2018-07-28 05:24:47 --> Helper loaded: url_helper
+INFO - 2018-07-28 05:24:47 --> Helper loaded: blog_helper
+INFO - 2018-07-28 05:24:47 --> Database Driver Class Initialized
+INFO - 2018-07-28 05:24:49 --> Controller Class Initialized
+DEBUG - 2018-07-28 05:24:49 --> Welcome MX_Controller Initialized
+DEBUG - 2018-07-28 05:24:49 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/Welcome/views/welcome_message.php
+INFO - 2018-07-28 05:24:49 --> Final output sent to browser
+DEBUG - 2018-07-28 05:24:49 --> Total execution time: 2.4500
+INFO - 2018-07-28 05:27:07 --> Config Class Initialized
+INFO - 2018-07-28 05:27:07 --> Hooks Class Initialized
+DEBUG - 2018-07-28 05:27:07 --> UTF-8 Support Enabled
+INFO - 2018-07-28 05:27:07 --> Utf8 Class Initialized
+INFO - 2018-07-28 05:27:07 --> URI Class Initialized
+DEBUG - 2018-07-28 05:27:07 --> No URI present. Default controller set.
+INFO - 2018-07-28 05:27:07 --> Router Class Initialized
+INFO - 2018-07-28 05:27:07 --> Output Class Initialized
+INFO - 2018-07-28 05:27:07 --> Security Class Initialized
+DEBUG - 2018-07-28 05:27:07 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-28 05:27:07 --> Input Class Initialized
+INFO - 2018-07-28 05:27:07 --> Language Class Initialized
+INFO - 2018-07-28 05:27:07 --> Language Class Initialized
+INFO - 2018-07-28 05:27:07 --> Config Class Initialized
+INFO - 2018-07-28 05:27:07 --> Loader Class Initialized
+INFO - 2018-07-28 05:27:07 --> Helper loaded: form_helper
+INFO - 2018-07-28 05:27:07 --> Helper loaded: url_helper
+INFO - 2018-07-28 05:27:07 --> Helper loaded: blog_helper
+INFO - 2018-07-28 05:27:07 --> Database Driver Class Initialized
+INFO - 2018-07-28 05:27:07 --> Controller Class Initialized
+DEBUG - 2018-07-28 05:27:07 --> Blog MX_Controller Initialized
+INFO - 2018-07-28 05:27:07 --> Model Class Initialized
+INFO - 2018-07-28 05:27:07 --> Model Class Initialized
+ERROR - 2018-07-28 05:27:08 --> Severity: Warning --> include(menu.php): failed to open stream: No such file or directory C:\xampp\htdocs\travelTipsBlog\public_html\application\modules\blog\views\show_entries.php 8
+ERROR - 2018-07-28 05:27:08 --> Severity: Warning --> include(): Failed opening 'menu.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\travelTipsBlog\public_html\application\modules\blog\views\show_entries.php 8
+DEBUG - 2018-07-28 05:27:08 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/blog/views/show_entries.php
+INFO - 2018-07-28 05:27:08 --> Final output sent to browser
+DEBUG - 2018-07-28 05:27:08 --> Total execution time: 1.1400
+INFO - 2018-07-28 05:31:47 --> Config Class Initialized
+INFO - 2018-07-28 05:31:47 --> Hooks Class Initialized
+DEBUG - 2018-07-28 05:31:47 --> UTF-8 Support Enabled
+INFO - 2018-07-28 05:31:47 --> Utf8 Class Initialized
+INFO - 2018-07-28 05:31:47 --> URI Class Initialized
+DEBUG - 2018-07-28 05:31:47 --> No URI present. Default controller set.
+INFO - 2018-07-28 05:31:47 --> Router Class Initialized
+INFO - 2018-07-28 05:31:47 --> Output Class Initialized
+INFO - 2018-07-28 05:31:47 --> Security Class Initialized
+DEBUG - 2018-07-28 05:31:47 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-07-28 05:31:47 --> Input Class Initialized
+INFO - 2018-07-28 05:31:47 --> Language Class Initialized
+INFO - 2018-07-28 05:31:47 --> Language Class Initialized
+INFO - 2018-07-28 05:31:47 --> Config Class Initialized
+INFO - 2018-07-28 05:31:47 --> Loader Class Initialized
+INFO - 2018-07-28 05:31:47 --> Helper loaded: form_helper
+INFO - 2018-07-28 05:31:47 --> Helper loaded: url_helper
+INFO - 2018-07-28 05:31:47 --> Helper loaded: blog_helper
+INFO - 2018-07-28 05:31:47 --> Database Driver Class Initialized
+INFO - 2018-07-28 05:31:47 --> Controller Class Initialized
+DEBUG - 2018-07-28 05:31:47 --> Blog MX_Controller Initialized
+INFO - 2018-07-28 05:31:47 --> Model Class Initialized
+INFO - 2018-07-28 05:31:47 --> Model Class Initialized
+DEBUG - 2018-07-28 05:31:47 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/blog/views/show_entries.php
+INFO - 2018-07-28 05:31:47 --> Final output sent to browser
+DEBUG - 2018-07-28 05:31:47 --> Total execution time: 0.0800
