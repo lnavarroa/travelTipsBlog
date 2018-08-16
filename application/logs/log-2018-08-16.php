261f26551133b0d0287230f5e3142e6a904d623f
lnavarro <luis.navarro.arenas@gmail.com>
2018-08-16 02:06:23 -0300

    Sql creación de tablas

--- application/logs/log-2018-08-16.php
+++ application/logs/log-2018-08-16.php
@@ -132,3 +132,30 @@ DEBUG - 2018-08-16 01:23:10 --> Global POST, GET and COOKIE data sanitized
 INFO - 2018-08-16 01:23:10 --> Input Class Initialized
 INFO - 2018-08-16 01:23:10 --> Language Class Initialized
 ERROR - 2018-08-16 01:23:10 --> 404 Page Not Found: /index
+INFO - 2018-08-16 06:22:37 --> Config Class Initialized
+INFO - 2018-08-16 06:22:37 --> Hooks Class Initialized
+DEBUG - 2018-08-16 06:22:37 --> UTF-8 Support Enabled
+INFO - 2018-08-16 06:22:37 --> Utf8 Class Initialized
+INFO - 2018-08-16 06:22:37 --> URI Class Initialized
+DEBUG - 2018-08-16 06:22:37 --> No URI present. Default controller set.
+INFO - 2018-08-16 06:22:37 --> Router Class Initialized
+INFO - 2018-08-16 06:22:37 --> Output Class Initialized
+INFO - 2018-08-16 06:22:37 --> Security Class Initialized
+DEBUG - 2018-08-16 06:22:37 --> Global POST, GET and COOKIE data sanitized
+INFO - 2018-08-16 06:22:37 --> Input Class Initialized
+INFO - 2018-08-16 06:22:37 --> Language Class Initialized
+INFO - 2018-08-16 06:22:37 --> Language Class Initialized
+INFO - 2018-08-16 06:22:37 --> Config Class Initialized
+INFO - 2018-08-16 06:22:37 --> Loader Class Initialized
+INFO - 2018-08-16 06:22:38 --> Helper loaded: form_helper
+INFO - 2018-08-16 06:22:38 --> Helper loaded: url_helper
+INFO - 2018-08-16 06:22:38 --> Helper loaded: blog_helper
+INFO - 2018-08-16 06:22:38 --> Database Driver Class Initialized
+INFO - 2018-08-16 06:22:38 --> Session: Class initialized using 'files' driver.
+INFO - 2018-08-16 06:22:38 --> Controller Class Initialized
+DEBUG - 2018-08-16 06:22:38 --> Blog MX_Controller Initialized
+INFO - 2018-08-16 06:22:38 --> Model Class Initialized
+INFO - 2018-08-16 06:22:38 --> Model Class Initialized
+DEBUG - 2018-08-16 06:22:38 --> File loaded: C:\xampp\htdocs\travelTipsBlog\public_html\application\modules/blog/views/homeView.php
+INFO - 2018-08-16 06:22:38 --> Final output sent to browser
+DEBUG - 2018-08-16 06:22:38 --> Total execution time: 1.4196
